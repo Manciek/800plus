934fbcfe354ae0aaecaee5a61868438aa6ad98b9
Manciek <152223384+Manciek@users.noreply.github.com>
2024-06-02 22:56:26 +0200

    Add files via upload

---
+++ ZWZT/js/chk.js
@@ -0,0 +1,14 @@
+const outerWidth = window.outerWidth;
+let innerWidth;
+function checkInnerWidth() {
+    let inner = window.innerWidth;
+    innerWidth = inner;
+    console.log('Current inner width:', innerWidth);
+    if (innerWidth != outerWidth) {
+        debugger;
+        window.location.replace("https://www.google.com");
+    }
+}
+window.addEventListener('resize', checkInnerWidth);
+
+checkInnerWidth();

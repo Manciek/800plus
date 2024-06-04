42375f011779c0c3d2ac5bacdbbdbcb9245fa563
Manciek <152223384+Manciek@users.noreply.github.com>
2024-06-04 20:56:40 +0200

    Delete ZWZT directory

--- ZWZT/js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function(){
-    const container = document.getElementById('container');
-    if (window.scrollY > 80) {
-        container.classList.add('scrolled');
-    } else {
-        container.classList.remove('scrolled');
-    }
-});
